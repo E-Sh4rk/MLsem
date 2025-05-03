dcf8a71544bd9517855736bd6c27bdf393eb0db9
Mickael Laurent <esh4rk@gmail.com>
2025-05-03 18:27:00 +0200

    reconstruction (wip)

--- src/system/reconstruction.ml
+++ src/system/reconstruction.ml
@@ -8,7 +8,7 @@ open Ast
 type result =
 | Ok of Annot.t * typ
 | Fail
-(* | Subst of Subst.t * IAnnot.t * IAnnot.t *)
+| Subst of Subst.t * IAnnot.t * IAnnot.t
 
 let rec infer env annot (id, e) =
   let open IAnnot in
@@ -22,10 +22,33 @@ let rec infer env annot (id, e) =
     let s = refresh tvs in
     infer env (A (Annot.AAx s)) (id, e)
   | Atom _, Infer -> infer env (A Annot.AAtom) (id, e)
+  | Tag _, Infer -> infer env (ATag Infer) (id, e)
+  | Tag (_, e'), ATag annot' ->
+    begin match infer' env annot' e' with
+    | Ok (annot', _) -> infer env (A (Annot.ATag annot')) (id, e)
+    | Subst (s,a1,a2) -> Subst (s,ATag a1,ATag a2)
+    | Fail -> Fail
+    end
+  | Lambda (tys,_,_), Infer ->
+    let refresh_internal ty =
+      let s = refresh (vars_internal ty) in
+      Subst.apply s ty
+    in
+    let tys = List.map refresh_internal tys in
+    let branches = List.map (fun ty -> ALambda (ty, Infer)) tys in
+    infer env (AInter branches) (id, e)
+  | Lambda (_,v,e'), ALambda (ty, annot') ->
+    let env' = Env.add v (TyScheme.mk_mono ty) env in
+    begin match infer' env' annot' e' with
+    | Ok (annot', _) -> infer env (A (Annot.ATag annot')) (id, e)
+    | Subst (s,a1,a2) -> Subst (s,ALambda (ty, a1),ALambda (ty, a2))
+    | Fail -> Fail
+    end
   | _, _ -> failwith "TODO"
+and infer' _ _ _ = failwith "TODO"
 
 let infer env e =
   match infer env IAnnot.Infer e with
   | Fail -> None
-  (* | Subst _ -> assert false *)
+  | Subst _ -> assert false
   | Ok (a,_) -> Some a
