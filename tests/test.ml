584bf4c86e08ad509a44e779c5a224b98b98c57a
Mickael Laurent <esh4rk@gmail.com>
2026-04-17 16:08:09 +0200

    another day another issue (actually the same day)

--- tests/test.ml
+++ tests/test.ml
@@ -1,20 +1,31 @@
 #value_restriction = false
 
-type rational_ = {
-  coerce: (A -> (A, A))
-        & (rational_ -> (rational_ , rational_))
-        & (complex  -> (rational_ , rational_) | (complex , complex))
-}
-and rational = rational_
+type _coerce_('other, 'converted_other, 'converted_self) = {
+  _name : ~(enum) ;
+  coerce: 'other -> ('converted_other , 'converted_self)
+..}
 
-and complex_ = {
-  coerce: any -> any
+type rational = {
+  _name: ~(Rational);
+  coerce: 
+          (rational -> (rational , rational))
+        & (complex  -> ((complex , complex)));
+  plus:   (any -> any)
 }
-and complex = complex_
+
+and complex = {
+  _name: ~(Complex);
+  coerce: any -> (complex , complex);
+  plus:
+     (rational -> complex)
+   & (_coerce_(complex, int, bool) -> any)
+}
 
 val magic : empty
 
-let a_rational = (
-  {
+val a_rational : rational
+let a_rational =
+  { _name = (Dummy :> ~(Rational)); 
+    plus = (fun x -> magic) ; 
     coerce =  (fun x -> magic)
-  } :> rational)
+  }
